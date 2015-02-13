ebdefdd600b67c728a625b621b84f72ed3566907
Leila Chan Currie <me@leila.cc>
2015-02-12 23:42:14 -0500

    set timeout

--- assets/js/fadein.js
+++ assets/js/fadein.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
   setTimeout(function(){}, 1000);
   $('#headerwrap').css('background', 'url(../img/shutterstock1920.jpg) no-repeat center top');
   $('#headerwrap').css('background-size', 'cover');
-  $('#headerwrap').show({opacity: 1}, 1000);
+  $('#headerwrap').animate({opacity: 1}, 3000);
 });
