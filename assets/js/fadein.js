bba25dbe75ef2b849396cdcc25b373fe3ac82d13
Leila Chan Currie <me@leila.cc>
2015-02-12 23:48:59 -0500

    set dummy

--- assets/js/fadein.js
+++ assets/js/fadein.js
@@ -1,5 +1,5 @@
 $('#dummy').ready(function() {
   $('#headerwrap').css('background', 'url(../img/shutterstock1920.jpg) no-repeat center top');
   $('#headerwrap').css('background-size', 'cover');
-  $('#headerwrap').fadeIn({opacity: 1}, 3000);
+  $('#headerwrap').fadeIn({opacity: 1}, 30000);
 });
