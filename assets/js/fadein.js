46bb8fdf875031002df1772ad88f254a1021d6be
Leila Chan Currie <me@leila.cc>
2015-02-13 00:02:01 -0500

    removed alert

--- assets/js/fadein.js
+++ assets/js/fadein.js
@@ -1,5 +1,4 @@
 $('#dummy').ready(function() {
-  alert('');
   $('#headerwrap').css('background', 'url(../img/shutterstock1920.jpg) no-repeat center top');
   $('#headerwrap').css('background-size', 'cover');
   $('#headerwrap').fadeIn("slow");
