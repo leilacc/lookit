1cc09e7c36ef8a723735874a4f38155b5745a98b
Leila Chan Currie <me@leila.cc>
2015-02-13 00:13:59 -0500

    removed mailchimp javascript

--- assets/js/fadein.js
+++ assets/js/fadein.js
@@ -1,5 +1,10 @@
 $('#dummy').ready(function() {
-  $('#headerwrap').css('background', 'url(../img/shutterstock1920.jpg) no-repeat center top');
+  if ($(window).width() > 1366) {
+    $('#headerwrap').css('background',
+      'url(../img/shutterstock1920.jpg) no-repeat center top');
+  } else {
+    $('#headerwrap').css('background',
+      'url(../img/shutterstock1366.jpg) no-repeat center top');
+  }
   $('#headerwrap').css('background-size', 'cover');
-  $('#headerwrap').fadeIn("slow");
 });
