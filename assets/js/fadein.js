58708687ad935e2ae69260db9bcbac4012a77bf3
Leila Chan Currie <me@leila.cc>
2015-02-12 23:36:24 -0500

    switch background to cover

--- assets/js/fadein.js
+++ assets/js/fadein.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
   $('#headerwrap').css('background', 'url(../img/shutterstock1920.jpg) no-repeat center top');
+  $('#headerwrap').css('background-size', 'cover');
   $('#headerwrap').show({opacity: 1}, 1000);
 });
