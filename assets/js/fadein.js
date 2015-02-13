4b9f5224a73a4db471e1cb443183ceb2a654f5c7
Leila Chan Currie <me@leila.cc>
2015-02-12 23:31:00 -0500

    breakpoint

--- assets/js/fadein.js
+++ assets/js/fadein.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  break;
   $('#headerwrap').css('background', 'url(../img/shutterstock1920.jpg) no-repeat center top');
-  $('#headerwrap').animate({opacity: 1}, 1000);
+  $('#headerwrap').show({opacity: 1}, 1000);
 });
